0191d3e33fca5c12a718afb57bd94ef8d9999b3f
Chris <chris@azurro.pl>
2023-03-06 16:15:41 +0100

    Refactoring

--- model.py
+++ model.py
@@ -210,12 +210,12 @@ class AllamoTransformer(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    def forward(self, idx, targets=None):
-        b, t = idx.size()
+    def forward(self, tokens, targets=None):
+        b, t = tokens.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
 
         # forward the model itself
-        tok_emb = self.tok_embeddings(idx) # token embeddings of shape (b, t, n_embd)
+        tok_emb = self.tok_embeddings(tokens) # token embeddings of shape (b, t, n_embd)
         self.freqs_cis = self.freqs_cis.to(tok_emb.device)
         freqs_cis = self.freqs_cis[:t]
         x = self.tok_drop(tok_emb)
@@ -279,17 +279,18 @@ class AllamoTransformer(nn.Module):
         return optimizer
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+    def generate(self, tokens, max_new_tokens, temperature=1.0, top_k=None):
         """
-        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        Take a conditioning sequence of tokens (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
         for _ in range(max_new_tokens):
             # if the sequence context is growing too long we must crop it at block_size
-            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
-            # forward the model to get the logits for the index in the sequence
-            logits, _ = self(idx_cond)
+            if tokens.size(1) <= self.config.block_size:
+                tokens = tokens[:, -self.config.block_size:]
+            # forward the model to get the logits for the tokens
+            logits, _ = self(tokens)
             # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / temperature
             # optionally crop the logits to only the top k options
@@ -299,8 +300,8 @@ class AllamoTransformer(nn.Module):
             # apply softmax to convert logits to (normalized) probabilities
             probs = F.softmax(logits, dim=-1)
             # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1)
-            # append sampled index to the running sequence and continue
-            idx = torch.cat((idx, idx_next), dim=1)
+            next_token = torch.multinomial(probs, num_samples=1)
+            # append next token to the running sequence and continue
+            tokens = torch.cat((tokens, next_token), dim=1)
 
-        return idx
+        return tokens
