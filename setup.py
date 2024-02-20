9d35129619f8c402b5a09da7213b6fdc6a55aff4
Chris <chris@azurro.pl>
2024-02-20 16:30:34 +0100

    Bump version to 2.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='allamo',
-      version='2.1.0',
+      version='2.2.0',
       author='Krzysztof (Chris) Ociepa',
       packages=['allamo'],
       description='Simple, hackable and fast implementation for training/finetuning medium-sized LLaMA-based models',
