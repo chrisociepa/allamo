03838edb6ae590d82955a5d87e70f6ac4e4d87b3
Chris <chris@azurro.pl>
2024-04-02 23:19:37 +0200

    Bump version to 4.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='allamo',
-      version='3.1.0',
+      version='4.0.0',
       author='Krzysztof (Chris) Ociepa',
       packages=['allamo'],
       description='Simple, hackable and fast implementation for training/finetuning medium-sized LLaMA-based models',
