Fix weighted loss calculation

fsdp_train.py
@@ -442,13 +442,9 @@ class AllamoFSDPTrainer:
                 logits, loss, _ = self.model(**batch)
                 if self.gradient_accumulation_steps > 1:
                     loss = loss / self.gradient_accumulation_steps # scale the loss to account for micro steps
-                    
                 if batch["target_weights"] is not None:
-                    fsdp_loss_weight_acc = batch["target_weights"].sum()
-                    # sum loss weights over all processes
-                    dist.all_reduce(fsdp_loss_weight_acc, op=dist.ReduceOp.SUM)
-                    loss = (self.world_size / fsdp_loss_weight_acc) * loss
-                
+                    loss = loss / torch.sum(batch["target_weights"] > 0).item()
+
                 mfu_excluded_time = time.time()
                 unmasked_labels = torch.sum(batch["target_ids"].view(-1) != self.config.ignore_index).item()
                 fsdp_loss_acc[0] += loss.item()

train.py
@@ -396,7 +396,7 @@ class AllamoTrainer:
                     if micro_steps > 1:
                         loss = loss / micro_steps # scale the loss to account for micro steps
                     if batch["target_weights"] is not None:
-                        loss = loss / batch["target_weights"].sum()
+                        loss = loss / torch.sum(batch["target_weights"] > 0).item()
                 
                 mfu_excluded_time = time.time()
                 unmasked_labels += torch.sum(batch["target_ids"].view(-1) != self.config.ignore_index).item()