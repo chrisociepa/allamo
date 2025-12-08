fcc5eecc64043b4036b71d4d91473d54ee85eb3c
Chris <chris@azurro.pl>
2025-12-08 15:38:27 +0100

    Bump version to 7.0.0

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='allamo',
-      version='6.0.0',
+      version='7.0.0',
       author='Krzysztof (Chris) Ociepa',
       packages=['allamo'],
-      description='Simple, hackable and fast implementation for training/finetuning medium-sized LLaMA-based models',
+      description='Simple, hackable and fast implementation for training and finetuning language models',
       license='MIT',
       install_requires=[
             'torch',
