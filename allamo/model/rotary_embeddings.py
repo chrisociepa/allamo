d198e82667668265d188d403c930e4b024f646ef
Chris <ociepa.krzysztof@gmail.com>
2026-04-21 00:14:34 +0200

    Fix applying input_pos to RotaryEmbedding

--- allamo/model/rotary_embeddings.py
+++ allamo/model/rotary_embeddings.py
@@ -144,8 +144,8 @@ class RotaryEmbedding(torch.nn.Module):
             cos = self.cos_cached[None, None, :q.size(2), ...]
             sin = self.sin_cached[None, None, :q.size(2), ...]
         else:
-            cos = self.cos_cached[input_pos].unsqueeze(1)
-            sin = self.sin_cached[input_pos].unsqueeze(1)
+            cos = self.cos_cached[input_pos][None, None, :, :]
+            sin = self.sin_cached[input_pos][None, None, :, :]
         
         q_out = (q * cos) + (self.__rotate_half(q) * sin)
         k_out = (k * cos) + (self.__rotate_half(k) * sin)
